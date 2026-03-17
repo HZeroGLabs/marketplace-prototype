b83ac32c55ff02ecd7dfaa6d427e8b32c55ab6cd
HZeroG <HZeroG@proton.me>
2026-03-17 01:59:46 -0400

    Completed listings CRUD with full database integration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from app.db.database import engine, Base
 from app.routers import users
+from app.routers import listings
 from fastapi import FastAPI
 from app.models import user
-from app.models import listings
+from app.models import listings as listings_model
 
 Base.metadata.create_all(bind=engine)
 
@@ -10,6 +11,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(listings.router)
 
 @app.get("/")
 def read_root():
---
+++ app/routers/listings.py
@@ -0,0 +1,58 @@
+from typing import List
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, APIRouter
+
+from app.db.database import get_db
+from app.models.listings import Listing as ListingModel
+from app.schemas.listing import Listing, ListingCreate
+
+router = APIRouter()
+
+
+@router.post("/listings", response_model=Listing)
+def create_listing(listing: ListingCreate, db: Session = Depends(get_db)):
+    db_listing = ListingModel(
+        title=listing.title,
+        description=listing.description,
+        price=listing.price,
+        owner_id=1,
+        )
+    
+    db.add(db_listing)
+    db.commit()
+    db.refresh(db_listing)
+    return db_listing
+
+@router.get("/listings", response_model=List[Listing])
+def get_listings(db: Session = Depends(get_db)):
+    listings = db.query(ListingModel).all()
+    return listings
+
+@router.get("/listings/{listing_id}", response_model=Listing)
+def get_listing(listing_id: int, db: Session = Depends(get_db)):
+    listing = db.query(ListingModel).filter(ListingModel.id == listing_id).first()
+    if not listing:
+        raise HTTPException(status_code=404, detail="Listing not found")
+    return listing
+
+@router.put("/listings/{listing_id}", response_model=Listing)
+def update_listing(listing_id: int, listing: ListingCreate, db: Session = Depends(get_db)):
+    db_listing = db.query(ListingModel).filter(ListingModel.id == listing_id).first()
+    if not db_listing:
+        raise HTTPException(status_code=404, detail="Listing not found")
+    
+    db_listing.title = listing.title
+    db_listing.description = listing.description
+    db_listing.price = listing.price
+    db.commit()
+    db.refresh(db_listing)
+    return db_listing
+
+@router.delete("/listings/{listing_id}", response_model=Listing)
+def delete_listing(listing_id: int, db: Session = Depends(get_db)):
+    db_listing = db.query(ListingModel).filter(ListingModel.id == listing_id).first()
+    if not db_listing:
+        raise HTTPException(status_code=404, detail="Listing not found")
+    db.delete(db_listing)
+    db.commit()
+    return db_listing
